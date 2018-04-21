e1e579ca2ee2661f84a3660ee5be61baad9fa495
Alvaro Escobar <alvaroescobaronline@gmail.com>
2018-04-21 10:01:37 -0400

    Initial Commit

---
+++ src/com/wgu/school/Roster.java
@@ -0,0 +1,24 @@
+package com.wgu.school;
+
+public class Roster {
+
+    public static void remove(String studentID){
+        //If the student ID doesn’t exist, the method should print an error message indicating that it is not found.
+
+    }
+
+    public static void print_all(){
+        //Tabs can be formatted as such: 1 [tab] First Name: John [tab] Last Name: Smith [tab] Age: 20 [tab] Grades: {88, 79, 59}
+
+    }
+
+    public static void print_average_grade(String studentID){
+
+    }
+
+    public static void print_invalid_emails(){
+        //A valid e-mail should include an at sign (“@”) and period (“.”) and doesn’t include a space.
+    }
+
+
+}
---
+++ src/com/wgu/school/Student.java
@@ -0,0 +1,13 @@
+package com.wgu.school;
+
+import java.lang.reflect.Array;
+
+public class Student {
+    int student_ID;
+    String first_name;
+    String last_name;
+    String email;
+    int age;
+    Array grades;
+
+}
