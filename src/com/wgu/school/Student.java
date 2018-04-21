a0da1952d2539ce13cfc9593b8db59e641709dc1
Alvaro Escobar <alvaroescobaronline@gmail.com>
2018-04-21 10:11:48 -0400

    Added getters and setters for Student

--- src/com/wgu/school/Student.java
+++ src/com/wgu/school/Student.java
@@ -3,11 +3,67 @@ package com.wgu.school;
 import java.lang.reflect.Array;
 
 public class Student {
-    int student_ID;
-    String first_name;
-    String last_name;
-    String email;
-    int age;
-    Array grades;
+    private int student_ID;
+    private String first_name;
+    private String last_name;
+    private String email;
+    private int age;
+    private Array grades;
 
+    public Student(int student_ID, String first_name, String last_name, String email, int age, Array grades) {
+        this.student_ID = student_ID;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.email = email;
+        this.age = age;
+        this.grades = grades;
+    }
+
+    public int getStudent_ID() {
+        return student_ID;
+    }
+
+    public void setStudent_ID(int student_ID) {
+        this.student_ID = student_ID;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Array getGrades() {
+        return grades;
+    }
+
+    public void setGrades(Array grades) {
+        this.grades = grades;
+    }
 }
